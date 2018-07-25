9b9a319131cad8971ef2c296b1d3daab7b215ce2
Atsushi Abe <piste@jp.ibm.com>
2018-07-25 13:38:18 +0900

    Fix memory leak at backtrace_info() (#64)

--- src/libltfs/ltfs_locking.h
+++ src/libltfs/ltfs_locking.h
@@ -87,18 +87,20 @@ enum {
 static inline void backtrace_info(void)
 {
 	void *address[50];
-	char **funcs;
+	char **funcs = NULL;
 	size_t back_num, i;
 
-	back_num = backtrace( address, 50);
+	back_num = backtrace(address, 50);
 	funcs = backtrace_symbols( address, back_num);
 
-	for( i = 0; i < back_num; ++i ) {
+	for(i = 0; i < back_num; ++i)  {
 		if (funcs && funcs[i])
 			ltfsmsg(LTFS_INFO, 17193I, (int)i, address[i], funcs[i]);
 		else
 			ltfsmsg(LTFS_INFO, 17194I, (int)i, address[i]);
 	}
+
+	if (funcs) free(funcs);
 
 	return;
 }
