64770a81cb1df91288e6b611c64fb4c0dcc9ed62
Atsushi Abe <piste@jp.ibm.com>
2020-03-16 16:23:13 +0900

    Update year of the copyright

--- replace_copyright.pl
+++ replace_copyright.pl
@@ -7,7 +7,7 @@ sub print_copyright
     @copyright = (
         "",
         "",
-        "Copyright 2010, 2019 IBM Corp. All rights reserved.",
+        "Copyright 2010, 2020 IBM Corp. All rights reserved.",
         "",
         "Redistribution and use in source and binary forms, with or without",
         " modification, are permitted provided that the following conditions",
--- src/utils/ltfs_ordered_copy
+++ src/utils/ltfs_ordered_copy
@@ -4,7 +4,7 @@
 #  OO_Copyright_BEGIN
 #
 #
-#  Copyright 2010, 2019 IBM Corp. All rights reserved.
+#  Copyright 2010, 2020 IBM Corp. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #   modification, are permitted provided that the following conditions
