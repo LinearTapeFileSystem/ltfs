15e50bc66c878002140ef3c77b6b4dace4b78ce0
Atsushi Abe <piste@jp.ibm.com>
2021-11-19 14:52:22 +0900

    Tiny correction for sync fail

--- src/libltfs/ltfs.c
+++ src/libltfs/ltfs.c
@@ -2380,7 +2380,7 @@ int ltfs_write_index(char partition, char *reason, struct ltfs_volume *vol)
 	bool generation_inc = false;
 	struct tc_position physical_selfptr;
 	char *cache_path_save = NULL;
-	bool write_perm = (strcmp(reason, SYNC_WRITE_PERM) == 0);
+	bool write_perm = (strcmp(reason, SYNCWRITE_PERM) == 0);
 	bool update_vollock = false;
 	int volstat = -1, new_volstat = 0;
 	char *bc_print = NULL;
@@ -3549,7 +3549,7 @@ start:
 			ret_r = ltfs_write_index(ltfs_ip_id(vol), SYNC_WRITE_PERM, vol);
 			if (!ret_r) {
 				ltfsmsg(LTFS_INFO, 11344I, bc_print);
-				ret = LTFS_SYNC_FAIL_ON_DP;
+				ret = -LTFS_SYNC_FAIL_ON_DP;
 			} else {
 				ltfsmsg(LTFS_ERR, 11345E, bc_print);
 				ltfsmsg(LTFS_ERR, 11346E, bc_print);
