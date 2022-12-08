835d21d1b74ffcd20d59dce38aab024da8afdd99
liu shiwei <liushiwei@gmail.com>
2022-12-08 13:06:46 +0800

    The software package libicu6x will be automatically installed.
    package icu-devtools depend libicu6x.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 
 sudo apt-get -q -y update
 sudo apt-get -q -y upgrade
-sudo apt-get -y install build-essential automake autoconf libtool pkg-config libicu66 icu-devtools libicu-dev libxml2-dev uuid-dev fuse libfuse-dev libsnmp-dev
+sudo apt-get -y install build-essential automake autoconf libtool pkg-config icu-devtools libicu-dev libxml2-dev uuid-dev fuse libfuse-dev libsnmp-dev
 ./autogen.sh
 ./configure
 make
