627484b2b0bbcdaac30281560a1a52601823336b
Missael Palacios <missaelpalacios@ibm.com>
2024-08-26 20:20:16 -0600

    Changed flags for _ltfs_search_index_wp

--- src/libltfs/ltfs.c
+++ src/libltfs/ltfs.c
@@ -1673,7 +1673,8 @@ int ltfs_mount(bool force_full, bool deep_recovery, bool recover_extra, bool rec
 						(unsigned long long)vol->dp_coh.volume_change_ref,
 						(unsigned long long)volume_change_ref);
 
-				ret = _ltfs_search_index_wp(recover_symlink, false, &seekpos, vol);
+				/* Index of IP could be corrupted. So set skip flag */
+				ret = _ltfs_search_index_wp(recover_symlink, true, &seekpos, vol);
 				if (ret < 0)
 					goto out_unlock;
 
@@ -1702,8 +1703,7 @@ int ltfs_mount(bool force_full, bool deep_recovery, bool recover_extra, bool rec
 						(unsigned long long)vol->dp_coh.volume_change_ref,
 						(unsigned long long)volume_change_ref);
 
-				/* Index of IP could be corrupted. So set skip flag */
-				ret = _ltfs_search_index_wp(recover_symlink, true, &seekpos, vol);
+				ret = _ltfs_search_index_wp(recover_symlink, false, &seekpos, vol);
 				if (ret < 0)
 					goto out_unlock;
 
