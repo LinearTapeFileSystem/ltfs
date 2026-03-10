8a419075c277c3ed0b83920ac5097ff7ba338797
Gustavo P. <47061318+Piloalucard@users.noreply.github.com>
2026-03-10 09:05:28 -0600

    Message in strncpy

--- src/libltfs/tape.c
+++ src/libltfs/tape.c
@@ -1782,6 +1782,7 @@ int tape_set_cart_coherency(struct device_data *dev, const tape_partition_t part
 	/* APPLICATION CLIENT SPECIFIC INFORMATION LENGTH */
 	coh_data[30] = 0;  /* Size of APPLICATION CLIENT SPECIFIC INFORMATION (Byte 1) */
 	coh_data[31] = 43; /* Size of APPLICATION CLIENT SPECIFIC INFORMATION (Byte 0) */
+	/* Size of the buffer to insert 'LTFS' needs to be size of 5 for the 4 letters and the null terminator*/
 	arch_strncpy((char *)coh_data + 32,"LTFS", 5, 4);
 	memcpy(coh_data + 37, coh->uuid, 37);
 	/*
